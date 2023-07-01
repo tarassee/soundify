cedd325c57356cc210a9e97fddec0d19e3dc8537
Taras Tarasiuk <taras_tarasiuk@epam.com>
2023-07-01 23:38:19 +0300

    [FIX] Moving the creation of MQ configs to the consumer side - Resolving the issue with failing resource-processor while listening non-existent queue during infrastructure startup. - The fix is to move the creation of MQ configs from resource-service(producer side) to resource-processor(consumer side).

--- soundify-resource-processor/src/main/java/com/tarasiuk/soundify/config/ConsumerMessagingConfig.java
+++ soundify-resource-processor/src/main/java/com/tarasiuk/soundify/config/ConsumerMessagingConfig.java
@@ -1,15 +1,44 @@
 package com.tarasiuk.soundify.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConsumerMessagingConfig {
+
+    @Value("${application.rabbitmq.queue}")
+    private String queue;
+    @Value("${application.rabbitmq.exchange}")
+    private String exchange;
+    @Value("${application.rabbitmq.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue)
+                .to(exchange)
+                .with(routingKey);
+    }
 
     @Bean
     public MessageConverter messageConverter() {
--- soundify-resource-service/src/main/java/com/tarasiuk/soundify/config/ProducerMessagingConfig.java
+++ soundify-resource-service/src/main/java/com/tarasiuk/soundify/config/ProducerMessagingConfig.java
@@ -1,40 +1,15 @@
 package com.tarasiuk.soundify.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProducerMessagingConfig {
-
-    @Value("${application.rabbitmq.queue}")
-    private String queue;
-    @Value("${application.rabbitmq.exchange}")
-    private String exchange;
-    @Value("${application.rabbitmq.routing-key}")
-    private String routingKey;
-
-    @Bean
-    public Queue queue() {
-        return new Queue(queue);
-    }
-
-    @Bean
-    public DirectExchange exchange() {
-        return new DirectExchange(exchange);
-    }
-
-    @Bean
-    public Binding binding(Queue queue, DirectExchange exchange) {
-        return BindingBuilder.bind(queue)
-                .to(exchange)
-                .with(routingKey);
-    }
 
     @Bean
     public MessageConverter messageConverter() {
